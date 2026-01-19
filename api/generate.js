79234152867c4e6fd25776f99a67238ce137b39f
NafisatB <nafisatbabamusa@outlook.com>
2026-01-19 06:01:29 +0100

    api folder created

--- api/generate.js
+++ api/generate.js
@@ -1,5 +1,3 @@
-import fetch from "node-fetch";
-
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method not allowed" });
@@ -30,8 +28,8 @@ export default async function handler(req, res) {
     );
 
     const data = await response.json();
-    res.status(200).json(data);
+    return res.status(200).json(data);
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    return res.status(500).json({ error: error.message });
   }
 }
